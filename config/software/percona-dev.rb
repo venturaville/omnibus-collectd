766623bfae0146deafa0962851d8fc6d49d38a7f
David Nicklay <david.nicklay@turner.com>
2014-05-02 13:41:58 -0400

    percona dev url fix

--- config/software/percona-dev.rb
+++ config/software/percona-dev.rb
@@ -3,7 +3,7 @@ default_version "5.5.33-rel31.1-566-static-openssl-1.0.1e"
 dependency "rsync"
 dependency "ncurses"
 
-source :url => "http://www.percona.com/downloads/Percona-Server-5.5/Percona-Server-5.5.33-31.1/binary/linux/x86_64/Percona-Server-5.5.33-rel31.1-566-static-openssl-1.0.1e.Linux.x86_64.tar.gz",
+source :url => "http://www.percona.com/redir/downloads/Percona-Server-5.5/Percona-Server-5.5.33-31.1/binary/linux/x86_64/Percona-Server-5.5.33-rel31.1-566.Linux.x86_64.tar.gz",
 :md5 => "1fe2c6596d9a1f0fb6f70e348edb89fc"
 
 relative_path "Percona-Server-5.5.33-rel31.1-566-static-openssl-1.0.1e.Linux.x86_64"
